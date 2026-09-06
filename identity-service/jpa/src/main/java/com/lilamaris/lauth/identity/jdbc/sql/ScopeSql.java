c2c9aff2b4a5e6445305f6b5fa108a8df4d6ff06
lilamaris <lilamaris.dev@gmail.com>
2026-09-06 18:51:17 +0900

    fix(identity-service): Scope 쿼리 누락 추가
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- identity-service/jpa/src/main/java/com/lilamaris/lauth/identity/jdbc/sql/ScopeSql.java
+++ identity-service/jpa/src/main/java/com/lilamaris/lauth/identity/jdbc/sql/ScopeSql.java
@@ -3,9 +3,15 @@ package com.lilamaris.lauth.identity.jdbc.sql;
 public class ScopeSql {
     public static final String INSERT = """
             INSERT INTO scope (
-            
+                resource,
+                action,
+                description,
+                created_at
             ) VALUES (
-            
+                :resource,
+                :action,
+                :description,
+                :createdAt
             )
             """;
 }
