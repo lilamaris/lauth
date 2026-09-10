2b76a74d2eb49e26afacaa45eb13a230a642a076
lilamaris <lilamaris.dev@gmail.com>
2026-09-10 16:01:59 +0900

    fix(identity-service): 사용하지 않는 Redis 구현체 제거
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- identity-service/redis/src/main/java/com/lilamaris/lauth/identity/redis/RefreshTokenRedisAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.lilamaris.lauth.identity.redis;
-
-import org.springframework.beans.factory.annotation.Qualifier;
-
-@Qualifier("cache")
-public class RefreshTokenRedisAdapter implements RefreshTokenReader {
-}
