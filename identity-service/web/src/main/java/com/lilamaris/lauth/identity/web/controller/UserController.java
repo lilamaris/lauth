29e1f99f65277a67d4a7b82548050c5156dc0faa
lilamaris <lilamaris.dev@gmail.com>
2026-09-11 10:22:47 +0900

    feat(identity-service): 사용자 핸들 변경 엔드포인트 추가
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

---
+++ identity-service/web/src/main/java/com/lilamaris/lauth/identity/web/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.lilamaris.lauth.identity.web.controller;
+
+import com.lilamaris.lauth.identity.application.model.user.UserPrincipal;
+import com.lilamaris.lauth.identity.application.port.in.UpdateHandleUseCase;
+import com.lilamaris.lauth.identity.application.port.in.result.UpdateHandleResult;
+import com.lilamaris.lauth.identity.web.controller.request.UpdateHandleRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UpdateHandleUseCase updateHandleUseCase;
+
+    @PreAuthorize("hasAuthority('SCOPE_user.write')")
+    @PatchMapping("/handle")
+    public ResponseEntity<UpdateHandleResult> updateHandle(
+            @Valid @RequestBody UpdateHandleRequest body,
+            @AuthenticationPrincipal(expression = "user") UserPrincipal user
+    ) {
+        var command = body.toCommand(user.userId());
+        var result = updateHandleUseCase.update(command);
+        return ResponseEntity.ok(result);
+    }
+}
