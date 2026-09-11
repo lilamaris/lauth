f3064795f765a43e3cfff578949a4e4d73c74a34
lilamaris <lilamaris.dev@gmail.com>
2026-09-11 10:22:21 +0900

    feat(identity-service): 사용자 핸들 변경 Jdbc 어댑터 구현
    Signed-off-by: lilamaris <lilamaris.dev@gmail.com>

--- identity-service/jpa/src/main/java/com/lilamaris/lauth/identity/jdbc/UserJdbcAdapter.java
+++ identity-service/jpa/src/main/java/com/lilamaris/lauth/identity/jdbc/UserJdbcAdapter.java
@@ -1,22 +1,26 @@
 package com.lilamaris.lauth.identity.jdbc;
 
 import com.lilamaris.lauth.identity.application.model.user.UserPrincipal;
+import com.lilamaris.lauth.identity.application.port.out.UserMetadataStore;
 import com.lilamaris.lauth.identity.application.port.out.UserPrincipalReader;
 import com.lilamaris.lauth.identity.application.port.out.UserStore;
+import com.lilamaris.lauth.identity.application.port.out.status.UpdateHandleStatus;
 import com.lilamaris.lauth.identity.domain.User;
 import com.lilamaris.lauth.identity.jdbc.row.UserRow;
 import com.lilamaris.lauth.identity.jdbc.sql.UserSql;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
-public class UserJdbcAdapter implements UserStore, UserPrincipalReader {
+public class UserJdbcAdapter implements UserStore, UserPrincipalReader, UserMetadataStore {
     private final JdbcClient jdbcClient;
 
     @Override
@@ -40,5 +44,20 @@ public class UserJdbcAdapter implements UserStore, UserPrincipalReader {
                 .param("updatedAt", Timestamp.from(user.getUpdatedAt()))
                 .query(UUID.class)
                 .single();
+    }
+
+    @Override
+    public UpdateHandleStatus updateHandle(UUID userId, String handle, Instant updatedAt) {
+        var sql = UserSql.UPDATE_HANDLE;
+        try {
+            var updated = jdbcClient.sql(sql)
+                    .param("userId", userId)
+                    .param("handle", handle)
+                    .param("updatedAt", Timestamp.from(updatedAt))
+                    .update() == 1;
+            return updated ? UpdateHandleStatus.UPDATED : UpdateHandleStatus.USER_NOT_FOUND;
+        } catch (DuplicateKeyException e) {
+            return UpdateHandleStatus.HANDLE_ALREADY_IN_USE;
+        }
     }
 }
